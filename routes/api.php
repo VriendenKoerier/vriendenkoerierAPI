f746e38569d532d3ec40d92691f1552e5e4fe703
Timon <timonvanwaardhuizen@gmail.com>
2019-12-10 11:05:04 +0100

    Oath added

--- routes/api.php
+++ routes/api.php
@@ -13,12 +13,31 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('package/{id}', 'PackageController@show');
+Route::get('packages', 'PackageController@index');
+
+Route::group(['middleware' => 'auth:api'], function()
+{
+    Route::post('package', 'PackageController@store');
+    Route::put('package', 'PackageController@store');
+    Route::delete('package/{id}', 'PackageController@destroy');
 });
 
-Route::get('packages', 'PackageController@index');
-Route::get('package/{id}', 'PackageController@show');
-Route::post('package', 'PackageController@store');
-Route::put('package', 'PackageController@store');
-Route::delete('package/{id}', 'PackageController@destroy');
+//Authentication fix
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+});
